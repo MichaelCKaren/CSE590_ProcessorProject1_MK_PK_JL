2a50f418ad65ce45049666fd75ce0a762d4d09b2
MichaelCKaren <michaelkaren1231@gmail.com>
2026-03-13 18:41:56 -0400

    Added Controller

---
+++ rtl/Controller.v
@@ -0,0 +1,104 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/13/2026 06:19:22 PM
+// Design Name: 
+// Module Name: Controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+`include "Defines.v"
+
+module Controller(
+    input wire [3:0] opcode,
+    input wire [3:0] funct,
+    output reg reg_wen,         // Register Write Enable, Active High
+    output reg op2_sel,         // Operand 2 Select For ALU, 0 = op2, 1 = imm_signed
+    output reg [1:0] alu_sel,   // ALU Operation Select, 0 = ADD, 1 = SUB, 2 = SLL, 3 = AND
+    output reg branch,          // Branch Enable, Active High
+    output reg jump,            // Jump Enable, Active High
+    output reg mem_wen,         // Memory Write Enable, Active High
+    output reg mem_ren,         // Memory Read Enable, Active High
+    output reg wb_sel           // Write Back Select, 0 = ALU Result, 1 = Memory Result
+    );
+    
+    // Begin Function Decoding
+    always @(*) begin
+        case (opcode)
+            `OP_R : begin
+                case (funct)
+                    `FUNCT_ADD : begin
+                        reg_wen = 1'b1;
+                        op2_sel = 1'b0;
+                        alu_sel = 2'd0;
+                        branch  = 1'b0;
+                        jump    = 1'b0;
+                        mem_wen = 1'b0;
+                        mem_ren = 1'b0;
+                        wb_sel  = 1'b0;
+                    end
+                    `FUNCT_SUB : begin
+                        
+                    end
+                    `FUNCT_SLL : begin
+                        
+                    end
+                    `FUNCT_AND : begin
+                        
+                    end
+                    default : begin
+                        reg_wen = 1'b0;
+                        op2_sel = 1'b0;
+                        alu_sel = 2'd0;
+                        branch  = 1'b0;
+                        jump    = 1'b0;
+                        mem_wen = 1'b0;
+                        mem_ren = 1'b0;
+                        wb_sel  = 1'b0;
+                    end
+                endcase
+            end
+            `OP_LW : begin
+               
+            end
+            `OP_SW : begin
+                
+            end
+            `OP_ADDI : begin
+                
+            end
+            `OP_BEQ : begin
+                
+            end
+            `OP_BNE : begin
+                
+            end
+            `OP_JMP : begin
+            
+            end
+            default : begin
+                reg_wen = 1'b0;
+                op2_sel = 1'b0;
+                alu_sel = 2'd0;
+                branch  = 1'b0;
+                jump    = 1'b0;
+                mem_wen = 1'b0;
+                mem_ren = 1'b0;
+                wb_sel  = 1'b0;
+            end
+        endcase
+    end
+    
+endmodule
--- rtl/Decoder.v
+++ rtl/Decoder.v
@@ -23,9 +23,11 @@
 
 module Decoder(
     input wire [15:0] instr_i,
+    input wire reg_wen,
+    input wire [15:0] reg_wdata,
     output wire [3:0] opcode_o,
-    output reg [15:0] op1_o,
-    output reg [15:0] op2_o,
+    output wire [15:0] op1_o,
+    output wire [15:0] op2_o,
     output wire [15:0] imm_o,
     output wire [11:0] j_addr_o
     );
@@ -45,48 +47,22 @@ module Decoder(
     // Fetch Register Data
     wire [15:0] rs = reg_file[rs_addr];
     wire [15:0] rt = reg_file[rt_addr];
-    wire [15:0] rd = rt;
     
+    assign op1_o = rs;
+    assign op2_o = rt;
+    
+
     // Parse Funct and IMM + Sign Extended
     wire [3:0] funct = instr_i[3:0];
     wire [3:0] imm = funct;
     wire [15:0] imm_signed = {{12{imm[3]}},imm};
     assign imm_o = imm_signed;
     
-    
-    // Begin Function Decoding
+    // Write Operation
     always @(*) begin
-        case (opcode)
-            `OP_R : begin
-                op1_o = rs;
-                op2_o = rt;
-            end
-            `OP_LW : begin
-            
-            end
-            `OP_SW : begin
-            
-            end
-            `OP_ADDI : begin
-            
-            end
-            `OP_BEQ : begin
-            
-            end
-            `OP_BNE : begin
-            
-            end
-            `OP_JMP : begin
-            
-            end
-            default : begin
-                op1_o = `ZeroWord;
-                op2_o = `ZeroWord;
-            end
-        endcase
+        if (reg_wen == 1'b1) begin
+            reg_file[rd_addr] = reg_wdata;
+        end
     end
-    
-    
-   
     
 endmodule
